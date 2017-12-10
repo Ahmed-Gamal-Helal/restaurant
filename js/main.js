fe5b6015a0955f977f764573f80afad0f1e8c2a6
Ahmed Gamal <ahmdtawfiek@gmail.com>
2017-12-10 22:12:54 +0200

    restaurant

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 $(function(){
     'use strict';
-    // slider adjustment
-    var x = $('.upper-bar').innerHeight(),
-        y = $('.navbar').innerHeight(),
-        z = $(window).height();
-    $('.slider, .carousel-item').height(z - (x + y));
+    $('.users').click(function(event) {
+        event.preventDefault();
+        var ahmed = document.querySelectorAll('.main-col .col-sm-3');
+        
+        for(var i = 3, ii = ahmed.length ; i < ii; i++) {
+            $(ahmed[i]).css('display', 'none');
+        }
+        var arr = ['المستخدمين', 'المجموعات', 'الصلاحيات'] ; 
+        for(var i = 0, ii = 3 ; i < ii; i++) {
+            ahmed[i].getElementsByTagName('h5')[0].innerHTML = arr[i];
+            ahmed[i].setAttribute('href','index-2.html');
+        }
+    });
+
+    $('#dataTable').DataTable();
 });
